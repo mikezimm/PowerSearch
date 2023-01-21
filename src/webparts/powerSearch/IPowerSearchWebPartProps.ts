27f4a2e673696d6a9f45975c3aac50e80fb76048
mikezimm <49648086+mikezimm@users.noreply.github.com>
2023-01-20 22:30:24 -0500

    Starting to re-add code from original project

---
+++ src/webparts/powerSearch/IPowerSearchWebPartProps.ts
@@ -0,0 +1,52 @@
+
+
+/***
+ * NOTE:  All imports in here Must be imported directly from fps-library-v2, not the fpsPreferences
+ * Or else it will get into an endless loop because these values are imported into fpsPreferences
+ * 
+ */
+import { IMinWPBannerProps } from '@mikezimm/fps-library-v2/lib/banner/interfaces/MinWP/IMinWPBannerProps';
+
+
+export const changePropertyGroupX : string[] = [ 'showSomeProps', 'showCustomProps' , 'showOOTBProps' , 'showApprovalProps' , 'propsTitleField', 'propsExpanded', 'selectedProperties' ];
+
+
+ /**
+  For props to export to panel but NOT save in analytics
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const WebPartAnalyticsChanges : any = {
+  groupX: changePropertyGroupX,
+}
+
+
+ /**
+ * These are properties to export BOTH to analytics AND the panel
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const WebPartPanelChanges : any = {
+
+}
+
+//Specific for this web part
+export const exportIgnorePropsWP : string[] = [ ];
+
+
+//These props will not be imported even if they are in one of the change arrays above (fail-safe)
+//This was done so user could not manually insert specific props to over-right fail-safes built in to the webpart
+
+//Specific for this web part
+export const importBlockPropsWP : string[] = [ 'showSomeProps' ];
+
+
+
+// export interface IFpsCore115BannerWebPartProps extends IMinWPBannerProps {
+  /**
+   * Extend with portions of FPS Props that are needed
+   * 
+   */
+export interface IPowerSearchWebPartProps extends IMinWPBannerProps {
+
+  description: string;
+
+}
--- src/webparts/powerSearch/PowerSearchWebPart.ts
+++ src/webparts/powerSearch/PowerSearchWebPart.ts
@@ -1,27 +1,132 @@
+/***
+ *    db    db  .d88b.       d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b .d8888. 
+ *    `8b  d8' .8P  Y8.        `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~' 88'  YP 
+ *     `8bd8'  88    88         88    88  88  88 88oodD' 88    88 88oobY'    88    `8bo.   
+ *       88    88    88         88    88  88  88 88~~~   88    88 88`8b      88      `Y8b. 
+ *       88    `8b  d8'        .88.   88  88  88 88      `8b  d8' 88 `88.    88    db   8D 
+ *       YP     `Y88P'       Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP    `8888Y' 
+ *         
+ *         
+ */
+
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
+  IPropertyPaneGroup,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 
+import { SPPermission, } from '@microsoft/sp-page-context';
+
+/***
+ *    d888888b db   db d888888b .d8888.      db   d8b   db d88888b d8888b.      d8888b.  .d8b.  d8888b. d888888b 
+ *    `~~88~~' 88   88   `88'   88'  YP      88   I8I   88 88'     88  `8D      88  `8D d8' `8b 88  `8D `~~88~~' 
+ *       88    88ooo88    88    `8bo.        88   I8I   88 88ooooo 88oooY'      88oodD' 88ooo88 88oobY'    88    
+ *       88    88~~~88    88      `Y8b.      Y8   I8I   88 88~~~~~ 88~~~b.      88~~~   88~~~88 88`8b      88    
+ *       88    88   88   .88.   db   8D      `8b d8'8b d8' 88.     88   8D      88      88   88 88 `88.    88    
+ *       YP    YP   YP Y888888P `8888Y'       `8b8' `8d8'  Y88888P Y8888P'      88      YP   YP 88   YD    YP    
+ *                               
+ *                               
+ */
+
+// STANDARD PROJECT IMPORTS
+
 import * as strings from 'PowerSearchWebPartStrings';
 import PowerSearch from './components/PowerSearch';
+import { IPowerSearchWebPartProps } from './IPowerSearchWebPartProps';
 import { IPowerSearchProps } from './components/IPowerSearchProps';
 
-export interface IPowerSearchWebPartProps {
-  description: string;
-}
+ /***
+  *     .o88b. .d8888. .d8888.      d8888b. d88888b  .d88b.  db    db d888888b d8888b. d88888b .d8888. 
+  *    d8P  Y8 88'  YP 88'  YP      88  `8D 88'     .8P  Y8. 88    88   `88'   88  `8D 88'     88'  YP 
+  *    8P      `8bo.   `8bo.        88oobY' 88ooooo 88    88 88    88    88    88oobY' 88ooooo `8bo.   
+  *    8b        `Y8b.   `Y8b.      88`8b   88~~~~~ 88    88 88    88    88    88`8b   88~~~~~   `Y8b. 
+  *    Y8b  d8 db   8D db   8D      88 `88. 88.     `8P  d8' 88b  d88   .88.   88 `88. 88.     db   8D 
+  *     `Y88P' `8888Y' `8888Y'      88   YD Y88888P  `Y88'Y8 ~Y8888P' Y888888P 88   YD Y88888P `8888Y' 
+  *
+  *     USED BY BANNER COMPONENTS
+  */
 
-export default class PowerSearchWebPart extends BaseClientSideWebPart<IPowerSearchWebPartProps> {
+import { initializeIcons } from '@uifabric/icons';
+initializeIcons();
+
+require('@mikezimm/fps-styles/dist/GrayPropPaneAccordions.css');
+require('@mikezimm/fps-styles/dist/FPSPinMe.css');
+require('@mikezimm/fps-styles/dist/FPSHeadings.css');
+require('@mikezimm/fps-styles/dist/PropPanelHelp.css');
+require('@mikezimm/fps-styles/dist/performance.css');
+
+
+/***
+*    d88888b d8888b. .d8888.      d8888b. d8888b. d88888b .d8888. d88888b d888888b .d8888. 
+*    88'     88  `8D 88'  YP      88  `8D 88  `8D 88'     88'  YP 88'     `~~88~~' 88'  YP 
+*    88ooo   88oodD' `8bo.        88oodD' 88oobY' 88ooooo `8bo.   88ooooo    88    `8bo.   
+*    88~~~   88~~~     `Y8b.      88~~~   88`8b   88~~~~~   `Y8b. 88~~~~~    88      `Y8b. 
+*    88      88      db   8D      88      88 `88. 88.     db   8D 88.        88    db   8D 
+*    YP      88      `8888Y'      88      88   YD Y88888P `8888Y' Y88888P    YP    `8888Y' 
+*
+*    USED IN PRESETTING PROPS
+*/
+
+import { PreConfiguredProps,  } from './CoreFPS/PreConfiguredSettings';
+
+import { PropertyPaneWebPartInformation } from '@pnp/spfx-property-controls/lib/PropertyPaneWebPartInformation';
+import { getAllDefaultFPSFeatureGroups } from '@mikezimm/fps-library-v2/lib/banner/propPane/AllDefaultFPSGroups';
+
+import { WebPartInfoGroup, } from '@mikezimm/fps-library-v2/lib/banner/propPane/WebPartInfoGroup';
+import { exportIgnorePropsWP, importBlockPropsWP, WebPartAnalyticsChanges, WebPartPanelChanges,  } from './IPowerSearchWebPartProps';
+import { gitRepoALVSearch } from '@mikezimm/fps-library-v2/lib/components/atoms/Links/LinksRepos';
+//  import { IFpsOldVsNewWebPartProps } from './IFpsOldVsNewWebPartProps';
+import { runFPSSuperOnInit } from '@mikezimm/fps-library-v2/lib/banner/FPSWebPartClass/runSuperOnInit';
+import { runFPSWebPartRender } from '@mikezimm/fps-library-v2/lib/banner/FPSWebPartClass/runWebPartRender';
+import { onFPSPropPaneCHanged } from '@mikezimm/fps-library-v2/lib/banner/FPSWebPartClass/runOnPropChange';
+import { FPSBaseClass } from '@mikezimm/fps-library-v2/lib/banner/FPSWebPartClass/FPSBaseClass';
+import { IThisFPSWebPartClass } from '@mikezimm/fps-library-v2/lib/banner/FPSWebPartClass/IThisFPSWebPartClass';
+
+
+export default class PowerSearchWebPart extends FPSBaseClass<IPowerSearchWebPartProps> {
 
   private _isDarkTheme: boolean = false;
   private _environmentMessage: string = '';
+  
+  protected async onInit(): Promise<void> {
+    this._environmentMessage = this._getEnvironmentMessage();
+
+    this._repoLink = gitRepoALVSearch; //Set as any but will get created in FPSSuperOnOnit
+    this._exportIgnorePropsWP = exportIgnorePropsWP;
+    this._importBlockPropsWP = importBlockPropsWP;
+    this._trickyApp = 'ALV Search';
+    this._trickyEmailsWP = []; // These are emails that get tricky functionality for this specific web part
+    this._allowShowSearch = false;  //Set to true if you want 'Toggle Search option' in property pane
+    this._allowSiteThemeChoice = true;  // Should be set true by default in fps-library-v2 1.0.78
+
+    return super.onInit().then(async _ => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      runFPSSuperOnInit( this as any, PreConfiguredProps, SPPermission );
+
+    });
+  }
+
+  /***
+   *    d8888b. d88888b d8b   db d8888b. d88888b d8888b.       .o88b.  .d8b.  db      db      .d8888. 
+   *    88  `8D 88'     888o  88 88  `8D 88'     88  `8D      d8P  Y8 d8' `8b 88      88      88'  YP 
+   *    88oobY' 88ooooo 88V8o 88 88   88 88ooooo 88oobY'      8P      88ooo88 88      88      `8bo.   
+   *    88`8b   88~~~~~ 88 V8o88 88   88 88~~~~~ 88`8b        8b      88~~~88 88      88        `Y8b. 
+   *    88 `88. 88.     88  V888 88  .8D 88.     88 `88.      Y8b  d8 88   88 88booo. 88booo. db   8D 
+   *    88   YD Y88888P VP   V8P Y8888D' Y88888P 88   YD       `Y88P' YP   YP Y88888P Y88888P `8888Y' 
+   *                  
+   *           Source:   PivotTiles 1.5.2.6                                                                                
+   */
 
   public render(): void {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const bannerProps = runFPSWebPartRender( this as any, strings, WebPartAnalyticsChanges, WebPartPanelChanges, );
+
     const element: React.ReactElement<IPowerSearchProps> = React.createElement(
       PowerSearch,
       {
@@ -34,12 +139,6 @@ export default class PowerSearchWebPart extends BaseClientSideWebPart<IPowerSear
     );
 
     ReactDom.render(element, this.domElement);
-  }
-
-  protected onInit(): Promise<void> {
-    this._environmentMessage = this._getEnvironmentMessage();
-
-    return super.onInit();
   }
 
   private _getEnvironmentMessage(): string {
@@ -76,23 +175,49 @@ export default class PowerSearchWebPart extends BaseClientSideWebPart<IPowerSear
     return Version.parse('1.0');
   }
 
+  /***
+ *    d8888b. d8888b.  .d88b.  d8888b.      d8888b.  .d8b.  d8b   db d88888b       .o88b. db   db  .d8b.  d8b   db  d888b  d88888b 
+ *    88  `8D 88  `8D .8P  Y8. 88  `8D      88  `8D d8' `8b 888o  88 88'          d8P  Y8 88   88 d8' `8b 888o  88 88' Y8b 88'     
+ *    88oodD' 88oobY' 88    88 88oodD'      88oodD' 88ooo88 88V8o 88 88ooooo      8P      88ooo88 88ooo88 88V8o 88 88      88ooooo 
+ *    88~~~   88`8b   88    88 88~~~        88~~~   88~~~88 88 V8o88 88~~~~~      8b      88~~~88 88~~~88 88 V8o88 88  ooo 88~~~~~ 
+ *    88      88 `88. `8b  d8' 88           88      88   88 88  V888 88.          Y8b  d8 88   88 88   88 88  V888 88. ~8~ 88.     
+ *    88      88   YD  `Y88P'  88           88      YP   YP VP   V8P Y88888P       `Y88P' YP   YP YP   YP VP   V8P  Y888P  Y88888P 
+ *                                                 
+ *                                                 
+ */
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected async onPropertyPaneFieldChanged(propertyPath: string, oldValue: any, newValue: any): Promise<void>{
+    super.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await onFPSPropPaneCHanged( this as any, propertyPath, oldValue, newValue );
+
+    this.context.propertyPane.refresh();
+
+    this.render();
+
+  }
+
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-this-alias
+    const thisAsAny: IThisFPSWebPartClass = this as any;
+
+    let groups: IPropertyPaneGroup[] = [ WebPartInfoGroup( this._repoLink, 'ALV Search Webpart', PropertyPaneWebPartInformation ) ];
+    const FPSGroups: IPropertyPaneGroup[] = getAllDefaultFPSFeatureGroups ( thisAsAny );
+
+    groups = [ ...groups, ...FPSGroups ];
+
+
     return {
       pages: [
         {
           header: {
             description: strings.PropertyPaneDescription
           },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
+          displayGroupsAsAccordion: true, //DONT FORGET THIS IF PROP PANE GROUPS DO NOT EXPAND
+          groups: groups,
         }
       ]
     };
